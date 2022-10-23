aa2f22ca41931c5706012d35e0775b3c49d1b6f3
Alvaro Andres Loaiza Duque <andresloaiza21@gmail.com>
2022-10-23 11:56:05 -0500

    configuracion incial del proyecto reserved

---
+++ src/main/java/com/reserved/ReservedApplication.java
@@ -0,0 +1,13 @@
+package com.reserved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservedApplication.class, args);
+	}
+
+}
